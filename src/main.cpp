50cffa7c97e8b695e795ed95c9acb28f51785b6f
tonarinoanoko <tonarinn00@gmail.com>
2025-05-18 21:21:30 +0900

    Cmake攻勢にC++のバージョン指定

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,48 @@
 #include <iostream>
 #include <DxLib.h>
+#include "System/Input/InputManager.h"
+#include "System/Input/MouseInputManager.h"
+#include "System/Log/Logger.h"
+#include "System/Scene/SceneManager.h"
+#include "System/Scene/DebugScene.h"
+#include "System/Scene/PauseScene.h"
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
-    // DxLibの初期化
-    if (DxLib_Init() == -1) {
-        return -1; // 初期化に失敗したら終了
-    }
-
-    // ウィンドウモードに変更
+    if (DxLib_Init() == -1) return -1;
     SetGraphMode(640, 480, 32);
     ChangeWindowMode(TRUE);
 
-    // メインループ
+    Log::Logger::getInstance().setLogLevel(Log::eLogLevel::Debug);
+    Scene::SceneManager& sceneManager = Scene::SceneManager::getInstance();
+    sceneManager.setPauseScene(std::make_unique<Scene::PauseScene>());
+
+    bool debugModeEnabled = false;
+
     while (!ProcessMessage()) {
+        Input::InputManager::getInstance().update();
+        Input::MouseInputManager::getInstance().update();
 
-        // 画面をクリア
+        if (Input::InputManager::getInstance().isKeyPressed(KEY_INPUT_D)) {
+            debugModeEnabled = !debugModeEnabled;
+            sceneManager.setDebugMode(debugModeEnabled);
+            LOGGER_INFO("Debug Mode : %s", debugModeEnabled ? "ON" : "OFF");
+        }
+
+        if (Input::InputManager::getInstance().isKeyPressed(KEY_INPUT_P) && !sceneManager.isFading()) {
+            sceneManager.isGamePaused() ? sceneManager.resumeGame() : sceneManager.pauseGame();
+        }
+
+        if (Input::InputManager::getInstance().isKeyPressed(KEY_INPUT_SPACE) && !sceneManager.isFading() && !sceneManager.isGamePaused()) {
+            sceneManager.startFadeOut(1.0f);
+        }
+
+        sceneManager.update();
+
         ClearDrawScreen();
-
-        // ここにゲームの描画処理などを記述します
-
-        // 画面を更新
+        sceneManager.draw();
         ScreenFlip();
     }
 
-    // DxLibの終了処理
     DxLib_End();
-
     return 0;
 }
